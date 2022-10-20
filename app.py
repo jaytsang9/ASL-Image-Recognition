b8a935a73bd4d56a6c580e87f2286a5b13ada120
Johnny Tsang <jaytsang9@gmail.com>
2022-10-20 00:18:29 -0400

    swap columns for sidebar

--- app.py
+++ app.py
@@ -1,64 +1,68 @@
 import streamlit as st
 from PIL import Image
+import torchvision
 from torchvision import transforms
 import torch
 import os
 from neural_net import prediction
 import time
+import numpy as np
 
 from matplotlib import pyplot as plt
 
-def make_predictions():
-    with st.spinner('Wait for it...'):
-        time.sleep(1)
-    
-    st.header('Top 3 Results')
-    for label, metric in zip(labels, metrics):
-        st.metric(label, f'{metric:.2f}%')
+st.title('ASL Alphabet Image Classifier')
 
-    st.success('Done!')
+tab_names = ["Take Photo", "Upload", "Learn ASL"]
+tab1, tab2, tab3 = st.tabs(tab_names)
 
-    
-
-
-st.title('ASL Alphabet Image Classifier', anchor=None)
-
-tab1, tab2 = st.tabs(["Upload","Learn ASL",])
-col1, col2 = st.columns(2)
 file_types = ['jpg', 'jpeg', 'png']
 
+def make_predictions(img):
+    labels, metrics = prediction(img) 
+    with st.spinner('Predicting...'):
+        time.sleep(0.7)
+        
+        st.header('Top 3 Results')
+        for label, metric in zip(labels, metrics):
+            st.metric(label, f'{metric:.2f}%')
+
+        st.success('Done!')
+
+def lay_columns(tab):
+    upload = None
+    if tab == "Take Photo":
+        upload = st.camera_input("Take a picture")
+    if tab == "Upload":
+        upload = st.file_uploader("Choose a file", type=file_types, accept_multiple_files=False)
+
+    if upload is not None:    
+        image = Image.open(upload).convert('RGB')
+        st.image(image, caption=f'{upload.name}', width=128)
+        with st.sidebar:
+            make_predictions(image)
+
 with tab1:
-    option = st.selectbox('Choose an Option:', ('Camera', 'Upload'))
-
-    with col1:
-        if option == 'Camera':
-            upload = st.camera_input("Take a picture")
-        elif option == 'Upload':
-            upload = st.file_uploader("Choose a file", type=file_types, accept_multiple_files=True)
-        else:
-            upload = None
-
-        if option == 'Upload' and upload is not None:
-            for u in upload:
-                image = Image.open(u).convert('RGB')
-                labels, metrics = prediction(image)
-                with col1:
-                    st.image(image, caption=f'{u.name}', width=256)
-                with col2:
-                    '''
-                    st.header('Top 3 Results')
-                    for label, metric in zip(labels, metrics):
-                        st.metric(label, f'{metric:.2f}%')
-                    '''
-                    make_predictions()
-
-        if option == 'Camera' and upload is not None:    
-            image = Image.open(upload).convert('RGB')
-            labels, metrics = prediction(image) 
-            with col2:
-                make_predictions()
-
+    #lay_columns(tab_names[0])
+    upload = st.camera_input("Take a picture")
+    if upload is not None:    
+        image = Image.open(upload).convert('RGB')
+        st.image(image, caption=f'{upload.name}', width=128)
+        with st.sidebar:
+            make_predictions(image)
 
 with tab2:
+    #lay_columns(tab_names[1])
+    upload = st.file_uploader("Choose a file", type=file_types, accept_multiple_files=False)
+
+    if upload is not None:    
+        image = Image.open(upload).convert('RGB')
+        st.image(image, caption=f'{upload.name}', width=128)
+        with st.sidebar:
+            make_predictions(image)
+
+with tab3:
     st.header("Learn the American Sign Language Alphabet")
     st.image("images/asl_alphabet.png", width=700)
+
+
+
