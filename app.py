0272e78574d0fc4ece8d5248279aff884e1e51c7
Johnny Tsang <jaytsang9@gmail.com>
2022-10-19 20:48:39 -0400

    add images folder

--- app.py
+++ app.py
@@ -8,45 +8,41 @@ from neural_net import prediction
 from matplotlib import pyplot as plt
 
 
-st.title('Welcome to ASL Image Recognition!')
+st.title('ASL Alphabet Image Classifier', anchor=None)
 
+tab1, tab2, tab3 = st.tabs(["Learn ASL", "Camera", "Upload"])
+col1, col2 = st.columns(2)
 file_types = ['jpg', 'jpeg', 'png']
 
-option = st.selectbox(
-     'Camera or Upload?',
-     ('Choose an Option', 'Camera', 'Upload'))
+with tab1:
+    st.header("Take a Picture")
+    cam_upload = st.camera_input("Take a picture")
+    img = Image.open(cam_upload).convert('RGB')
+    labels, metrics = prediction(img) 
+    with col1:
+        st.image(img, caption=f'{cam_upload.name}', width=256)
+    with col2:
+        st.header('Top 3 Results')
+        for label, metric in zip(labels, metrics):
+            st.metric(label, f'{metric:.2f}%')
 
-if option == 'Camera':
-    upload = st.camera_input("Take a picture")
-elif option == 'Upload':
+
+with tab2:
+    st.header("Upload an Image")
     upload = st.file_uploader("Choose a file", type=file_types, accept_multiple_files=True)
-else:
-    upload = None
 
+    for u in upload:
+        img = Image.open(u).convert('RGB')
+        labels, metrics = prediction(img)
+    with col1:
+        st.image(img, caption=f'{u.name}', width=256)
+    with col2:
+        st.header('Top 3 Results')
+        for label, metric in zip(labels, metrics):
+            st.metric(label, f'{metric:.2f}%')
+        
 
+with tab3:
+    st.header("Learn the American Sign Language Alphabet")
+    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
 
-# upload = st.camera_input("Take a picture")
-col1, col2 = st.columns(2)
-
-if upload is not None:
-    # To read file as bytes:
-    if option == 'Upload':
-        for u in upload:
-            image = Image.open(u).convert('RGB')
-            labels, metrics = prediction(image)
-            with col1:
-                st.image(image, caption=f'{u.name}', width=256)
-            with col2:
-                st.header('Top 3 Results')
-                for label, metric in zip(labels, metrics):
-                    st.metric(label, f'{metric:.2f}%')
-
-    else:
-        image = Image.open(upload).convert('RGB')
-        labels, metrics = prediction(image) 
-        with col1:
-            st.image(image, caption=f'{upload.name}', width=256)
-        with col2:
-            st.header('Top 3 Results')
-            for label, metric in zip(labels, metrics):
-                st.metric(label, f'{metric:.2f}%')
